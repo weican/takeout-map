0b1193dc95aacb63e5de130e97916249f0239434
Wales <pc963kimo@gmail.com>
2020-04-27 18:23:50 -0600

    fix: disable password

--- src/services/Geocoding.tsx
+++ src/services/Geocoding.tsx
@@ -1,10 +1,9 @@
 import { sendGetRequest } from "./Request"
-import apikey from '../apikey.json';
 
 const url = "https://maps.googleapis.com/maps/api/geocode/json";
 
 export const getPlaceData = (address: string) => {
     const regexp = /#[0-9]+/;
     const modifyAddress = address.replace(regexp, '');
-    return sendGetRequest(`${url}?address=${modifyAddress}&key=${apikey.key}`);
+    return sendGetRequest(`${url}?address=${modifyAddress}`);
 }
